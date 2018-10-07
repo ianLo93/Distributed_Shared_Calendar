240a4d4bad149ecedb61703b5f468483aa89e9cf
ianLo93 <ianlo930627@gmail.com>
2018-10-07 16:01:20 -0400

    template

---
+++ src/com/project1/app/MyApp.java
@@ -0,0 +1,35 @@
+package com.example.app;
+
+import java.util.*;
+import com.example.client.Module1;
+import com.example.server.Module2;
+
+public class MyApp {
+
+  public static void main(String args[]){
+
+    if(args.length < 1){
+        System.out.println("ERROR: Please include 1 argument");
+        System.exit(1);
+    }
+    Module1 a = new Module1(args[0]);
+    Module2 b = new Module2(101);
+
+    a.printit();
+    b.printit();
+    b.printModule3();
+
+    ArrayList<String> lis = new ArrayList<String>();
+    lis.add("Testing");
+    lis.add("importing");
+    lis.add("a");
+    lis.add("foreign");
+    lis.add("module.");
+
+    for(String item : lis){
+      System.out.println(item);
+    }
+
+  }
+
+}
