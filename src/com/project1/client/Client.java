1175ffae571e2d559aee9a25bbb991c2d4e8583b
ywrsusan <ywrsusan@outlook.com>
2018-10-08 01:17:32 -0400

    meeting class and functions

--- src/com/project1/client/Client.java
+++ src/com/project1/client/Client.java
@@ -1,14 +1,39 @@
 package com.project1.client;
+import java.util.*;
+import com.project1.meeting.Meeting;
+
 
 public class Client {
-  String str;
 
-  public Client(String s){
-    this.str = s;
-  }
+    public String getSite_id() {
+        return site_id;
+    }
 
-  public void printit(){
-    System.out.println("My string is " + str);
-  }
+
+
+    String site_id;
+
+    ArrayList<Meeting> events;
+
+    ArrayList<String> T;
+
+    ArrayList<Integer> avaible;
+
+
+
+
+    public Client(String id, Integer n){
+        this.site_id = id;
+        this.avaible = new ArrayList<Integer>(Collections.nCopies(60, 0));
+        char[] chars = new char[n];
+        Arrays.fill(chars, '0');
+        String s = new String(chars);
+        this.T = new ArrayList<String>(Collections(n), s);
+    }
+
+
+
+
+    public void printit(){ System.out.println("My id is " + site_id); }
 
 }
